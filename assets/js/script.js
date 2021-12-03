b6dc37d4e12f0a193dc7524058664504d9e4705e
Guillermo Lazo <glazovg@gmail.com>
2021-12-02 21:40:43 -0600

    Refactoring timeOver and adding goBack and resetHighscores functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,21 @@ let timeEl = document.getElementById("time-left");
 let getScoresBtn = document.querySelectorAll("#options button")[0];
 let startBtn = document.querySelectorAll("#options button")[1];
 let scoreSection = document.querySelector(".score-section");
-let scoreBoardSection = document.querySelector(".score-board-section");
 let scoreEl = document.querySelector(".score-section p");
 let initialsInputEl = document.getElementById("initials-input");
 let saveScoreBtn = document.getElementById("save-score");
+let scoreBoardSection = document.querySelector(".score-board-section");
+let scoreList = document.querySelector(".score-list");
+let goBackBtn = document.getElementById("back-btn");
+let resetBtn = document.getElementById("reset-score-btn");
 let quizSection = document.querySelector(".quiz");
 let questionCard = document.createElement("div");
 let questionEl = document.createElement("p");
 let answerList = document.createElement("ul");
 let gradeEl = document.createElement("p");
-let timeOverEl = document.createElement("img");
 let totalQuestions = Object.keys(quiz).length;
 
 questionCard.classList.add('question-card');
-timeOverEl.src = "assets/img/time-is-over.png";
-timeOverEl.alt = "Time is Over Image";
 
 let qn = 1;
 let secondsLeft = 225;
@@ -42,7 +42,7 @@ function startTimer() {
         secondsLeft--;
         timeEl.textContent = `Time: ${secondsLeft}`;
 
-        if (secondsLeft === 0) {
+        if (secondsLeft === 0 || secondsLeft < 0) {
             // Stops execution of action at set interval
             clearInterval(timerInterval);
             timeOver();
@@ -114,13 +114,18 @@ function removeChilds(parent) {
 
 //timeOver function when time runs out elements are removed and replace for time over image
 function timeOver() {
-    setTimeout(function () {
-        questionCard.removeChild(gradeEl);
-        questionCard.removeChild(questionEl);
-        questionCard.removeChild(answerList);
-        questionCard.appendChild(timeOverEl);
-    }, 1000);
+    timeEl.classList.add("hide")
+    if (document.querySelector('.grade')) questionCard.removeChild(gradeEl);
+    questionCard.removeChild(questionEl);
+    questionCard.removeChild(answerList);
 
+    let timeOverTitle = document.createElement("h3");
+
+    timeOverTitle.textContent = "Time is Over"
+
+    scoreSection.insertBefore(timeOverTitle, document.querySelector(".score-title"));
+
+    //timeEl.classList.add("hide")
     scoreForm();
 }
 
@@ -148,15 +153,34 @@ function storeScore(event) {
 function getScores() {
     let lastScore = JSON.parse(localStorage.getItem("quizScore"));
 
-    scoreSection.classList.add("hide");
-    scoreBoardSection.classList.remove("hide")
+    if (instructions) instructions.classList.add("hide");
 
-    if (lastScore.totalPoints !== null) {
-        document.querySelector(".score").textContent = `${lastScore.initials}  -  ${lastScore.totalPoints}`
+    scoreSection.classList.add("hide");
+    scoreBoardSection.classList.remove("hide");
+
+    if (lastScore !== null) {
+        document.querySelector(".score").textContent = `${lastScore.initials}  -  ${lastScore.totalPoints}`;
+    } else {
+        document.querySelector(".score").classList.add("hide");
     }
 }
 
-//On Start Quiz clicked
+function goBack() {
+    window.location.reload();
+}
+
+function resetHighscores() {
+    localStorage.clear();
+    document.querySelector(".score").classList.add("hide");
+}
+
+//Clicking get Scores button
 getScoresBtn.addEventListener("click", getScores);
+//Clicking Score Board button
 startBtn.addEventListener("click", startQuiz);
+//Clicking Saving score button
 saveScoreBtn.addEventListener("click", storeScore);
+//Clicking Go back button
+goBackBtn.addEventListener("click", goBack);
+//Clicking Reset Highscores
+resetBtn.addEventListener("click", resetHighscores);
